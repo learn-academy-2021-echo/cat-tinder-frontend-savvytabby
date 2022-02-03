b43cf26fe1841e75885af26268de9adb6599f0e7
hawk198723 <hawk198723@gmail.com>
2022-02-02 17:13:29 -0800

    tesing with Jest and Enzyme

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,29 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import React from 'react'
+import Enzyme, { shallow } from 'enzyme'
+import Adapter from 'enzyme-adapter-react-16'
+import App from './App'
+import Home from './pages/Home'
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+//Allows us to utilize the adapter we import in earlier, allowing us to call and render a component.
+Enzyme.configure({adapter: new Adapter()})
+
+describe("When App renders", () => {
+  it("displays a header and footer", () => {
+    //Arrange
+    const renderedApp = shallow(<App/>)
+    //variable that class on shallow which is passed an arguemnt
+    const renderedHeader = renderedApp.find("Header")
+    const renderedFooter = renderedApp.find("Footer")
+    //Assert
+    expect(renderedHeader.length).toEqual(1)
+    expect(renderedFooter.length).toEqual(1)
+  })
+  it("provideing a route'/' to the home component",() => {
+      const renderedApp = shallow(<App/>)
+      const renderedHomeRoute = renderedApp.find('[path="/"]')
+      console.log("Rendered Home Debug", renderedHomeRoute.debug());
+      console.log("Rendered Home Props", renderedHomeRoute.props());
+      expect(renderedHomeRoute.length).toEqual(1)
+      expect(renderedHomeRoute.props().component).toEqual(Home)
+  })
+})
